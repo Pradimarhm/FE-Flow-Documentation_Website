69a9ecd0c13a9cb975df7dd93850dd52303a2f24
mnhurr <hudamohammadnur1987@gmail.com>
2026-07-20 15:08:12 +0700

    test commit

--- src/components/layouts/authLayout.jsx
+++ src/components/layouts/authLayout.jsx
@@ -9,7 +9,7 @@ export default function AuthLayout() {
             FlowDoc
           </h2>
           <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-            Masuk untuk mengakses kanvas simulasimu
+            Masuk untuk mengakses Ivan
           </p>
         </div>
         
